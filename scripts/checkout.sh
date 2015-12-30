bd01bf262858824c9f519155ef9a672c4bec6631
Joe Yennaco <joe@joeyennaco.com>
2015-12-30 12:43:52 -0500

    Added instructions in the script comments

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -21,6 +21,7 @@
 
 mkdir -p ${ASSET_DIR}/src
 
+# Update the svn checkout URL to your source code repository on Software Forge
 # Its a good idea to use the exit code to tell CONS3RT if the checkout succeeded
 
 svn checkout --non-interactive https://svn.forge.mil/svn/repos/cons3rt-sourcecode-sample/HelloWorld ${ASSET_DIR}/src
